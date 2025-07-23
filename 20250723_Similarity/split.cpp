fcf8b51b8d9727d1261c941fb4e438da8e955f66
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 15:35:43 +0900

    [feature] Len exception, partial scroe

--- 20250723_Similarity/split.cpp
+++ 20250723_Similarity/split.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 class Similarity {
 public:
-	float checkLenSimilarity(const string& str1, const string& str2) {
+	double checkLenSimilarity(const string& str1, const string& str2) {
 		int len1 = str1.length();
 		int len2 = str2.length();
+
+		if (len1 < 1) { throw length_error("String length must be larger than 0"); }
+		if (len2 < 1) { throw length_error("String length must be larger than 0"); }
 		
-		int gap = abs(len1 - len2);
-		float score = (1 - gap / min(len1, len2)) * 60;
+		double gap = abs(len1 - len2);
+		double score = (1 - gap / min(len1, len2)) * 60;
 
 		if (max(len1, len2) >= 2 * min(len1, len2)) score = 0;
 
--- 20250723_Similarity/split_test.cpp
+++ 20250723_Similarity/split_test.cpp
@@ -19,6 +19,21 @@ TEST_F(SimilarityFixture, CheckTextLength2) {
 	EXPECT_EQ(0, ret);
 }
 
+TEST_F(SimilarityFixture, CheckTextLength3) {
+	try {
+		ret = similarity.checkLenSimilarity("A", "");
+		FAIL();
+	}
+	catch (const exception& e) {
+
+	}
+}
+TEST_F(SimilarityFixture, CheckTextLength4) {
+	ret = similarity.checkLenSimilarity("AAABB", "BAA");
+	EXPECT_EQ(20, ret);
+}
+
+
 int main() {
 	InitGoogleMock();
 	return RUN_ALL_TESTS();
