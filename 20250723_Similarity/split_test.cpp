953ebed9f8a4e43748e0d5ccab9798984046ef17
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 16:26:35 +0900

    [refactoring] Refer to instructor's code

--- 20250723_Similarity/split_test.cpp
+++ 20250723_Similarity/split_test.cpp
@@ -3,43 +3,21 @@
 
 using namespace testing;
 
-struct TestTarget {
-	string str1;
-	string str2;
-	double score;
-};
-
 class SimilarityFixture : public Test {
 public:
 	Similarity similarity;
-	TestTarget target;
 	double ret;
 
-	void checkTextLength(TestTarget target) {
-		ret = similarity.getLenSimilarity(target.str1, target.str2);
-		EXPECT_DOUBLE_EQ(target.score, ret);
+	void checkTextLength(double score, string str1, string str2) {
+		ret = similarity.getLenSimilarity(str1, str2);
+		EXPECT_DOUBLE_EQ(score, ret);
 	}
 };
 
 TEST_F(SimilarityFixture, CheckTextLength) {
-	target = { "ASD", "DSA", 60 };
-	checkTextLength(target);
-
-	target = { "A", "BB", 0 };
-	checkTextLength(target);
-	
-	target = { "AAABB", "BAA", 20 };
-	checkTextLength(target);	
-}
-
-TEST_F(SimilarityFixture, CheckTextLengthAssertion) {
-	try {
-		ret = similarity.getLenSimilarity("A", "");
-		FAIL();
-	}
-	catch (const std::exception& e) {
-
-	}
+	checkTextLength(60, "ASD", "DSA");
+	checkTextLength(0, "A", "BB");
+	checkTextLength(20, "AAABB", "BAA");
 }
 
 int main() {
