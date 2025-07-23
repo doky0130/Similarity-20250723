7b605248edf1f188d8f7741b5a636e2721301957
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 15:26:59 +0900

    [refactoring] TestFixture

--- 20250723_Similarity/split_test.cpp
+++ 20250723_Similarity/split_test.cpp
@@ -3,15 +3,19 @@
 
 using namespace testing;
 
-TEST(TS, CheckTextLength) {
+class SimilarityFixture : public Test {
+public:
 	Similarity similarity;
-	float ret = similarity.checkLenSimilarity("ASD", "DSA");
+	float ret;
+};
+
+TEST_F(SimilarityFixture, CheckTextLength) {
+	ret = similarity.checkLenSimilarity("ASD", "DSA");
 	EXPECT_EQ(60, ret);
 }
 
-TEST(TS, CheckTextLength2) {
-	Similarity similarity;
-	float ret = similarity.checkLenSimilarity("A", "BB");
+TEST_F(SimilarityFixture, CheckTextLength2) {
+	ret = similarity.checkLenSimilarity("A", "BB");
 	EXPECT_EQ(0, ret);
 }
 
